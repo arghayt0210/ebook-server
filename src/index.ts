7049e81905deb800b49d6e7a9604a9272354c880
arghayt0210 <argha.yt.0210@gmail.com>
2024-10-30 08:47:14 +0530

    created basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-import { add } from "@/test/math";
+import express from "express";
 
-console.log("this: ", add(8, 10));
+const app = express();
+
+const port = process.env.PORT || 8989;
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.listen(port, () => {
+  console.log(`The application is running on port http://localhost:${port}`);
+});
